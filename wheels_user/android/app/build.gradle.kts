567230fcc089ae54b4a649da11159393ef18cfd9
nikhil <nikhil.kusuri@signitives.net>
2026-08-31 16:19:31 +0530

    Set Android applicationId to com.example.wheels_rider to authorize Google Maps API Key

--- wheels_user/android/app/build.gradle.kts
+++ wheels_user/android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.wheels_user"
+        applicationId = "com.example.wheels_rider"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
